25ee9b822bd69fed8914f9119583efd35db1bd83
한재현 <ultimate931218@gmail.com>
2020-04-28 15:58:35 +0900

    라우터 이동이 아닌 app.vue 변경

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <!--src/App.vue-->
 <template>
   <div id="app">
-    <task-list />
+    <inbox-screen />
   </div>
 </template>
 
 <script>
 import store from "./store";
-import TaskList from "./components/TaskList.vue";
-
+import InboxScreen from "./components/InboxScreen.vue";
 export default {
   name: "app",
   store,
   components: {
-    TaskList,
+    InboxScreen,
   },
 };
 </script>
